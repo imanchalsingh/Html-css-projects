23aba7c08c6fc1f0729a88efc3036bfcbb7e7b68
anchalDev159357 <singhanchal159357@gmail.com>
2024-12-24 17:47:14 +0530

    Added projects

---
+++ alphabate-table/script.js
@@ -0,0 +1,8 @@
+const alphabate = document.querySelectorAll(".alpha");
+const Explain = document.querySelectorAll(".Explain");
+
+for (let i = 0; i < Explain.length; i++) {
+  Explain[i].addEventListener('onclick' ,function(){
+  });
+
+}
